71dc6dbd13ee8fe5f66c60a13f8df2985010dce9
wangyao <1770624149@qq.com>
2020-11-19 21:22:02 +0800

    用户注册、用户登录、用户修改个人信息

---
+++ application/all/controller/Upload.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace app\all\controller;
+
+
+class Upload
+{
+    /**单图片上传
+     * 2020.11.19   王瑶
+     * @param $file
+     * @return mixed
+     */
+    public function uploadavatar($file)
+    {
+        // 移动到框架应用根目录/uploads/ 目录下
+        $info = $file->move( '../uploads');
+        if($info){
+            // 成功上传后 获取上传信息
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            return $info->getSaveName();
+        }else{
+            // 上传失败获取错误信息
+            return $file->getError();
+        }
+    }
+}
