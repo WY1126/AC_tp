3cb8620e3ac23bb2bbed19867a6066cab6fff333
wangyao <1770624149@qq.com>
2020-11-23 16:54:26 +0800

    社团详细信息

--- application/association/controller/Association.php
+++ application/association/controller/Association.php
@@ -14,7 +14,12 @@ class Association
      */
     public function getallassociation()
     {
-        return json(AssociatonModel::select());
+        $count = AssociatonModel::count('aid');
+
+        return json([
+            'count'     =>      $count,
+            'data'      =>      AssociatonModel::select(),
+        ]);
     }
 
     /**获取我的社团
@@ -27,7 +32,6 @@ class Association
         $uid = $request -> post('uid');
         //column获取某一列的值
         $aid = AssociatorModel::where('uid',$uid)->column('aid');
-//        return json($aid);
         if(!$aid)
         {
             return json([
@@ -35,8 +39,46 @@ class Association
                 'msg'           =>      '无加入的社团',
             ]);
         }
-        $association = AssociatonModel::whereIn('aid',$aid)->select();
-        return json($association);
+        //whereIn直接匹配数组值
+        $associations = AssociatonModel::whereIn('aid',$aid)->select();
+        return json($associations);
+    }
+
+    /**通过ID获取社团信息
+     * 2020.11.23 16:29     王瑶
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getassociation($aid)
+    {
+        $association = AssociatonModel::where('aid',$aid)->findOrEmpty();
+        if(!$association)
+        {
+            return json([
+                'error_code'        =>      1,
+                'msg'               =>      '错误',
+            ]);
+        }
+        return $association;
+    }
+    public function getanum($aid)
+    {
+        $num = AssociatorModel::where('aid',$aid)->count();
+        if(!$num)
+        {
+            $num=0;
+        }
+        return $num;
+    }
+    //进入社团详情页，获取详细信息
+    public function associationinfo(Request $request)
+    {
+        $aid = $request -> post('aid');
+        //获取社团协会基本信息
+        $association = $this->getassociation($aid);
+        //获取社团人数、点赞数
+        $num = AssociatorModel::where('aid',$aid)->count();
     }
 
 }
