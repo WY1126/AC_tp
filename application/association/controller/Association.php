014abb1f1b0108cfe15bc38832c9964471eabf55
wangyao <1770624149@qq.com>
2020-11-23 15:44:27 +0800

    获取我的社团

--- application/association/controller/Association.php
+++ application/association/controller/Association.php
@@ -16,13 +16,27 @@ class Association
     {
         return json(AssociatonModel::select());
     }
+
+    /**获取我的社团
+     * 2020.11.23 王瑶
+     * @param Request $request
+     * @return \think\response\Json
+     */
     public function getmyassociation(Request $request)
     {
         $uid = $request -> post('uid');
-//        return json($uid);
-        $aid = AssociatorModel::where('uid',$uid)->value('aid');
-        return json($aid);
-//        return json(AssociatonModel::hasWhere('aid',$aid)->find());
+        //column获取某一列的值
+        $aid = AssociatorModel::where('uid',$uid)->column('aid');
+//        return json($aid);
+        if(!$aid)
+        {
+            return json([
+                'error_code'    =>      1,
+                'msg'           =>      '无加入的社团',
+            ]);
+        }
+        $association = AssociatonModel::whereIn('aid',$aid)->select();
+        return json($association);
     }
 
 }
